7016b6ccffda9a5e7b3cb66e9d34bc6703b8cd57
KiKiKi <chaika.design@gmail.com>
2020-07-18 05:23:18 +0900

    Implement signup.

---
+++ api/routes/auth.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace MyApp\Routes;
+
+require_once __DIR__ . '/../config.php';
+require_once DB_DIR . '/users.php';
+require_once LIB_DIR . '/auth.php';
+
+use MyApp\DataBase\Users as DB_Users;
+use MyApp\Auth as JWT;
+use \Exception;
+
+class Auth
+{
+  /**
+   * Sign up handler
+   */
+  public static function signup()
+  {
+    $username = trim(filter_input(INPUT_POST, 'username'));
+    $password = trim(filter_input(INPUT_POST, 'password'));
+    $email = trim(filter_input(INPUT_POST, 'email'));
+
+    // Validation
+    $errors = self::validate_signup_data($username, $email, $password);
+    if (!empty($errors)) {
+      self::send_validation_error($errors, [
+        'username' => $username,
+        'email'    => $email,
+        'password' => $password,
+      ]);
+    }
+
+    try {
+      $data = DB_Users\create_user($username, $email, $password);
+
+      // create JWT token
+      $token = JWT\create_jwt_token([
+        'id' => $data['id'],
+      ]);
+
+      return_json([
+        'data'  => $data,
+        'token' => $token,
+      ]);
+    } catch (Exception $e) {
+      self::send_error($e);
+    }
+  }
+
+
+  /**
+   * Validate signup form data
+   */
+  private static function validate_signup_data($username, $email, $password)
+  {
+    $errors = [];
+
+    if (empty($username)) {
+      $errors['username'] = 'Please enter user name.';
+    } else if (!preg_match('/^([\w\-]+)$/', $username)) {
+      $errors['username'] = 'User name can include alphabets, `-` and `_`.';
+    }
+
+    if (empty($password)) {
+      $errors['password'] = 'Please enter a password';
+    } else if (strlen($password) !== mb_strlen($password)) {
+      $errors['password'] = 'Enter the password in single-byte characters.';
+    } else if (preg_match("/\s+/", $password)) {
+      $errors['password'] = 'Password cannot contain spaces.';
+    } else if (strlen($password) < 6) {
+      $errors['password'] = 'Please enter a password with at least 6 characters.';
+    }
+
+    if (empty($email)) {
+      $errors['email'] = 'Please enter your E-mail';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $errors['email'] = 'The E-mail is invalid';
+    } else {
+      // check email already used.
+      try {
+        $userData = DB_Users\get_user_by_email($email);
+        if (!empty($userData)) {
+          $errors['email'] = 'Thie E-mail is already registered.';
+        }
+      } catch (Exception $e) {
+        $errors['error'] = $e->getMessage();
+      }
+    }
+    return $errors;
+  }
+
+  // Send validation error.
+  private static function send_validation_error($errors, $data = null)
+  {
+    return_json([
+      'message' => 'validation error',
+      'errors' => $errors,
+      'data' => $data,
+    ], false, 200);
+  }
+
+  // return Error
+  private static function send_error($e)
+  {
+    $status = 500;
+    $error = $e->getMessage();
+    return_json(['message' => $error], false, $status);
+  }
+}
