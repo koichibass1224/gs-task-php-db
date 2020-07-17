5f7b3e0214de7cbf8a8e1da6df008e204c301e84
KiKiKi <chaika.design@gmail.com>
2020-07-18 05:17:29 +0900

    Create JWT token & verify JWT token methods.

--- api/config.php
+++ api/config.php
@@ -20,3 +20,20 @@ define('DB_TABLE_USERS', 'users');
 define('DB_TABLE_POSTS', 'posts');
 define('DB_TABLE_TAGS', 'tags');
 define('DB_TABLE_POST_TAG_RELATION', 'post_tag_relationships');
+
+// JWT
+define('JWT_HEADER', 'HTTP_' . $_ENV['JWT_HEADER']);
+
+define('JWT_PRIVATE_KEY', $_ENV['JWT_PRIVATE_KEY']);
+define('JWT_HASH_ALG', $_ENV['JWT_HASH_ALG']);
+// iss 発行者の識別子
+define('JWT_ISS', $_ENV['JWT_ISS']);
+// aud このTokenを利用することが想定される対象の識別子
+define('JWT_AUD', $_ENV['JWT_AUD']);
+// exp JWTの有効期限: 秒
+define('JWT_EXP', $_ENV['JWT_EXP']);
+
+// JWT refresh token
+define('JWT_REF_PRIVATE_KEY', $_ENV['JWT_REF_REF_EXP']);
+// exp
+define('JWT_REF_EXP', $_ENV['JWT_REF_EXP']);
