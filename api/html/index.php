a142f183d545824f633d1faf37b20fa40b195350
KiKiKi <chaika.design@gmail.com>
2020-07-24 02:13:53 +0900

    Modify API to return status code when Bad request.

--- api/html/index.php
+++ api/html/index.php
@@ -39,11 +39,13 @@ $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
   case FastRoute\Dispatcher::NOT_FOUND:
     // ... 404 Not Found
+    http_response_code(404);
     echo '404 Not Found';
     break;
   case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
     $allowedMethods = $routeInfo[1];
     // ... 405 Method Not Allowed
+    http_response_code(405);
     echo '405 Method Not Allowed';
     break;
   case FastRoute\Dispatcher::FOUND:
